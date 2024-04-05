afc174a33bccc95a370bc92c0aa5f52e613ab077
nnlgsakib <nlgarts@outlook.com>
2024-04-05 15:50:05 +0600

    init

---
+++ precache-manifest.8f9686d02cf9146278330456b37bd1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3414f74ea4ad7b51901b96c099e09b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a60d9111358adc9783",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "bddfd4ccca42ff491ca7",
+    "url": "/static/js/2.c0001eff.chunk.js"
+  },
+  {
+    "revision": "aaf168b94062fd5eddc2bafce074e7d6",
+    "url": "/static/js/2.c0001eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a60d9111358adc9783",
+    "url": "/static/js/main.64ec463b.chunk.js"
+  },
+  {
+    "revision": "9d5c1ed763fee7d9fcb9",
+    "url": "/static/js/runtime-main.1923f15f.js"
+  }
+]);
